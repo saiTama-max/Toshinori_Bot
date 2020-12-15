57d1e11e360df52a42b2e28739aab64aef7fa1d8
saiTama-max <vinamraj01@gmail.com>
2020-12-15 12:18:58 +0530

    Added graph command to see graph on top message count

--- cogs/utility.py
+++ cogs/utility.py
@@ -12,6 +12,7 @@ from dateutil.relativedelta import relativedelta
 import asyncpg
 from cogs.currency import USERNAME, HOST, DATABASE, PASSWORD
 import asyncio
+from matplotlib import pyplot as plt
 
 
 QUOTES = [
@@ -61,212 +62,270 @@ class Database:
 
 class Utility(commands.Cog):
 
-	def __init__(self, bot: commands.bot):
-		self.bot = bot
+    def __init__(self, bot: commands.bot):
+        self.bot = bot
 
-	
-	@staticmethod
-	def get_time(a: datetime.datetime, b: datetime.datetime) -> list:
-		try:
-			final = []
-			delta = relativedelta(a, b)
-			years = abs(delta.years)
-			months = abs(delta.months)
-			days = abs(delta.days)
-			hours = abs(delta.hours)
-			minutes = abs(delta.minutes)
+    
+    @staticmethod
+    def get_time(a: datetime.datetime, b: datetime.datetime) -> list:
+        try:
+            final = []
+            delta = relativedelta(a, b)
+            years = abs(delta.years)
+            months = abs(delta.months)
+            days = abs(delta.days)
+            hours = abs(delta.hours)
+            minutes = abs(delta.minutes)
 
-			if minutes and not months and not years:
-				final.append(f'{minutes} minutes')
+            if minutes and not months and not years:
+                final.append(f'{minutes} minutes')
 
-			if hours:
-				final.append(f'{hours} hours')
+            if hours:
+                final.append(f'{hours} hours')
 
-			if days:
-				final.append(f'{days} days')
+            if days:
+                final.append(f'{days} days')
 
-			if months:
-				final.append(f'{months} months')
-			
-			if years:
-				final.append(f'{years} years')
-			final = final[::-1]
-			return final[:3]
-		except Exception as e:
-			print(e)
+            if months:
+                final.append(f'{months} months')
+            
+            if years:
+                final.append(f'{years} years')
+            final = final[::-1]
+            return final[:3]
+        except Exception as e:
+            print(e)
 
-	@commands.command(brief='Get an answer to a query through the wolfram api', usage='t!wolfram [query]')
-	async def wolfram(self, ctx, *, query):
-	    APPID = "AA9R93-WY7A2ARPR7"
-	    QUERY = "http://api.wolframalpha.com/v2/{request}?{data}"
-
-
-
-	    url_str = parse.urlencode({
-	        "i": query,
-	        "appid": APPID,
-	    })
-
-	    query_final = QUERY.format(request="simple", data=url_str)
-
-	    async with aiohttp.ClientSession() as cs:
-	        async with cs.get(query_final) as response:
-	            image_bytes = await response.read()
+    @commands.command(brief='Get an answer to a query through the wolfram api', usage='t!wolfram [query]')
+    async def wolfram(self, ctx, *, query):
+        APPID = "AA9R93-WY7A2ARPR7"
+        QUERY = "http://api.wolframalpha.com/v2/{request}?{data}"
 
 
-	            f = discord.File(BytesIO(image_bytes), filename="image.png")
-	            image_url = "attachment://image.png"
+
+        url_str = parse.urlencode({
+            "i": query,
+            "appid": APPID,
+        })
+
+        query_final = QUERY.format(request="simple", data=url_str)
+
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(query_final) as response:
+                image_bytes = await response.read()
 
 
-	            final_emb = discord.Embed(color=discord.Color.orange())
-	            final_emb.set_image(url=image_url)
-	            
-	            await ctx.send(embed=final_emb, file=f)
+                f = discord.File(BytesIO(image_bytes), filename="image.png")
+                image_url = "attachment://image.png"
 
 
-	@commands.command(brief='gives nice and motivational codes', usage='t!quote')
-	async def quote(self, ctx):
-	    embed_quote = discord.Embed(title="Quote", description=random.choice(QUOTES), color=discord.Color.green())
-	    await ctx.send(embed=embed_quote)
+                final_emb = discord.Embed(color=discord.Color.orange())
+                final_emb.set_image(url=image_url)
+                
+                await ctx.send(embed=final_emb, file=f)
+
+
+    @commands.command(brief='gives nice and motivational codes', usage='t!quote')
+    async def quote(self, ctx):
+        embed_quote = discord.Embed(title="Quote", description=random.choice(QUOTES), color=discord.Color.green())
+        await ctx.send(embed=embed_quote)
         
-	@commands.command(brief='Get the latency of the bot', usage='t!ping')
-	async def ping(self, ctx):
-	    ping_emb = discord.Embed(title="Pong! 🏓",
-	     						 description=f'Ping is {round(self.bot.latency  * 1000)}ms',
-	     						 color=discord.Color.blurple())
-	    await ctx.send(embed=ping_emb)
+    @commands.command(brief='Get the latency of the bot', usage='t!ping')
+    async def ping(self, ctx):
+        ping_emb = discord.Embed(title="Pong! 🏓",
+                                 description=f'Ping is {round(self.bot.latency  * 1000)}ms',
+                                 color=discord.Color.blurple())
+        await ctx.send(embed=ping_emb)
 
-	@commands.command(brief='Get the info about the user',
-					usage='t!userinfo [user (optional)]',
-					aliases=['ui', 'user', 'uinfo', 'useri'])
-	async def userinfo(self, ctx, user: discord.Member=None):
+    @commands.command(brief='Get the info about the user',
+                    usage='t!userinfo [user (optional)]',
+                    aliases=['ui', 'user', 'uinfo', 'useri'])
+    async def userinfo(self, ctx, user: discord.Member=None):
 
-		hypesquad = {'hypesquad_bravery': '<:bravery:784430728683323392>',
-        			 'hypesquad_brilliance': '<:brilliance:784430705118806036>',
-        			 'hypesquad_balance': '<:balance:784430681857720332>',}
-		try:
-			user = ctx.author if not user else user
-			pfp = user.avatar_url
-			created_time = self.get_time(datetime.datetime.utcnow(), user.created_at) 
-			badges = user.public_flags
+        hypesquad = {'hypesquad_bravery': '<:bravery:784430728683323392>',
+                     'hypesquad_brilliance': '<:brilliance:784430705118806036>',
+                     'hypesquad_balance': '<:balance:784430681857720332>',}
+        try:
+            user = ctx.author if not user else user
+            pfp = user.avatar_url
+            created_time = self.get_time(datetime.datetime.utcnow(), user.created_at) 
+            badges = user.public_flags
 
-			joined_time = self.get_time(datetime.datetime.utcnow(), user.joined_at)
-			info_emb = discord.Embed(color=user.color,
-			 						 title=str(user))
-			if badges.hypesquad_bravery:
-				info_emb.description = hypesquad['hypesquad_bravery']
+            joined_time = self.get_time(datetime.datetime.utcnow(), user.joined_at)
+            info_emb = discord.Embed(color=user.color,
+                                     title=str(user))
+            if badges.hypesquad_bravery:
+                info_emb.description = hypesquad['hypesquad_bravery']
 
-			elif badges.hypesquad_brilliance:
-				info_emb.description = hypesquad['hypesquad_brilliance']
+            elif badges.hypesquad_brilliance:
+                info_emb.description = hypesquad['hypesquad_brilliance']
 
-			elif badges.hypesquad_balance:
-				info_emb.description = hypesquad['hypesquad_balance']
+            elif badges.hypesquad_balance:
+                info_emb.description = hypesquad['hypesquad_balance']
 
-			roles = []
+            roles = []
 
-			for i in user.roles[1:]:
-				roles.append(f"<@&{i.id}>")
-			roles = ', '.join(roles)
+            for i in user.roles[1:]:
+                roles.append(f"<@&{i.id}>")
+            roles = ', '.join(roles)
 
-			statuses = {discord.Status.online: '<:online:782636673653407744>',
-						discord.Status.offline: '<:offline:782636856075616286>',
-						discord.Status.idle: '<:3929_idle:782638272483950623>',
-						discord.Status.dnd: '<:dnd:782637101464027136>'}
+            statuses = {discord.Status.online: '<:online:782636673653407744>',
+                        discord.Status.offline: '<:offline:782636856075616286>',
+                        discord.Status.idle: '<:3929_idle:782638272483950623>',
+                        discord.Status.dnd: '<:dnd:782637101464027136>'}
 
-			info_emb.set_thumbnail(url=pfp)
-			info_emb.add_field(name='User Information', value=f'Created: {", ".join(created_time)} ago\nProfile: {user.mention}\nID: {user.id}', inline=False)
-			info_emb.add_field(name="Guild Profile", value=f"Joined: {', '.join(joined_time)} ago\nNickname: {user.nick}\nRoles: {roles}", inline=False)
-			info_emb.add_field(name="Status", value=f'{statuses[user.mobile_status]} Mobile Client\n{statuses[user.web_status]} Web Client\n{statuses[user.desktop_status]} Desktop Client')
+            info_emb.set_thumbnail(url=pfp)
+            info_emb.add_field(name='User Information', value=f'Created: {", ".join(created_time)} ago\nProfile: {user.mention}\nID: {user.id}', inline=False)
+            info_emb.add_field(name="Guild Profile", value=f"Joined: {', '.join(joined_time)} ago\nNickname: {user.nick}\nRoles: {roles}", inline=False)
+            info_emb.add_field(name="Status", value=f'{statuses[user.mobile_status]} Mobile Client\n{statuses[user.web_status]} Web Client\n{statuses[user.desktop_status]} Desktop Client')
 
-			await ctx.send(embed=info_emb)
-		except Exception as e:
-			print(e)
-	db = Database()
-	@commands.Cog.listener()
-	async def on_message(self, message):
-		async with self.db as conn:
-			await conn.execute("""CREATE TABLE IF NOT EXISTS quirks (
-				username text,
-				quirk text,
-				c_spins integer,
-				uc_spins integer,
-				r_spins integer,
-				userid bigint,
-				current timestamptz,
-				messages integer,
-				guild bigint,
-				yen bigint)""")
+            await ctx.send(embed=info_emb)
+        except Exception as e:
+            print(e)
+    db = Database()
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        async with self.db as conn:
+            await conn.execute("""CREATE TABLE IF NOT EXISTS quirks (
+                username text,
+                quirk text,
+                c_spins integer,
+                uc_spins integer,
+                r_spins integer,
+                userid bigint,
+                current timestamptz,
+                messages integer,
+                guild bigint,
+                yen bigint)""")
 
-			if message.author == self.bot.user:
-				return
-			try:
-				if conn:
-					check = await conn.fetchrow("SELECT userid FROM quirks WHERE userid=$1", message.author.id)
-					if not check:
-						await conn.execute("INSERT INTO quirks(userid, guild, messages) VALUES($1, $2, 1)",
-									 	   message.author.id, message.guild.id)
-					
-					else:
-						guild = await conn.fetch("SELECT guild FROM quirks WHERE userid=$1", message.author.id)
-						guild = [list(i.values()) for i in guild]
-						msg_count = await conn.fetchrow("SELECT messages FROM quirks WHERE userid=$1 AND guild=$2",
-														message.author.id, message.guild.id)
-						if msg_count:
-							msg_count = list(msg_count.values())[0] if list(msg_count.values())[0] else 0
-						else:
-							msg_count = 0
-						msg_count += 1
-						if [message.guild.id] in guild:
-							await conn.execute("UPDATE quirks SET messages=$3 WHERE userid=$1 AND guild=$2",
-										 message.author.id, message.guild.id, msg_count)
-						else:
-							await conn.execute("INSERT INTO quirks(userid, guild, messages) VALUES($1, $2, 1)",
-									 	   message.author.id, message.guild.id)
-					# await conn.close()
-			except Exception as e:
-				print(e)
-			
+            if message.author == self.bot.user:
+                return
+            try:
+                if conn:
+                    check = await conn.fetchrow("SELECT userid FROM quirks WHERE userid=$1", message.author.id)
+                    if not check:
+                        await conn.execute("INSERT INTO quirks(userid, guild, messages) VALUES($1, $2, 1)",
+                                           message.author.id, message.guild.id)
+                    
+                    else:
+                        guild = await conn.fetch("SELECT guild FROM quirks WHERE userid=$1", message.author.id)
+                        guild = [list(i.values()) for i in guild]
+                        msg_count = await conn.fetchrow("SELECT messages FROM quirks WHERE userid=$1 AND guild=$2",
+                                                        message.author.id, message.guild.id)
+                        if msg_count:
+                            msg_count = list(msg_count.values())[0] if list(msg_count.values())[0] else 0
+                        else:
+                            msg_count = 0
+                        msg_count += 1
+                        if [message.guild.id] in guild:
+                            await conn.execute("UPDATE quirks SET messages=$3 WHERE userid=$1 AND guild=$2",
+                                         message.author.id, message.guild.id, msg_count)
+                        else:
+                            await conn.execute("INSERT INTO quirks(userid, guild, messages) VALUES($1, $2, 1)",
+                                           message.author.id, message.guild.id)
+                    # await conn.close()
+            except Exception as e:
+                print(e)
+            
 
-	@commands.command(brief="Show the top for people with the most messages in a guild",
-					  usage="t!top",
-					  aliases=('lb', 'leaderboard'))
-	async def top(self, ctx):
-		async with self.db as conn:
-			await conn.execute("""CREATE TABLE IF NOT EXISTS quirks (
-				username text,
-				quirk text,
-				c_spins integer,
-				uc_spins integer,
-				r_spins integer,
-				userid bigint,
-				current timestamptz,
-				messages integer,
-				guild bigint,
-				yen bigint)""")
-			msgs = await conn.fetch("SELECT userid, messages FROM quirks WHERE guild=$1", ctx.guild.id)
-			msgs = set(msgs)
-			m_count = dict()
-			for i in msgs:
-				m_count.update({str(self.bot.get_user(list(i.values())[0])): list(i.values())[1]})
-			m_count = {k: m_count[k] for k in sorted(m_count, key=lambda y: m_count[y])}
-			msg_count = tuple(m_count.values())[::-1]
-			name = tuple(m_count.keys())[::-1]
+    @commands.command(brief="Show the top for people with the most messages in a guild",
+                      usage="t!top",
+                      aliases=('lb', 'leaderboard'))
+    async def top(self, ctx):
+        async with self.db as conn:
+            await conn.execute("""CREATE TABLE IF NOT EXISTS quirks (
+                username text,
+                quirk text,
+                c_spins integer,
+                uc_spins integer,
+                r_spins integer,
+                userid bigint,
+                current timestamptz,
+                messages integer,
+                guild bigint,
+                yen bigint)""")
+            msgs = await conn.fetch("SELECT userid, messages FROM quirks WHERE guild=$1", ctx.guild.id)
+            msgs = set(msgs)
+            m_count = dict()
+            for i in msgs:
+                m_count.update({str(self.bot.get_user(list(i.values())[0])): list(i.values())[1]})
+            m_count = {k: m_count[k] for k in sorted(m_count, key=lambda y: m_count[y])}
+            msg_count = tuple(m_count.values())[::-1]
+            name = tuple(m_count.keys())[::-1]
 
-			fin = []
-			c = 1
-			
-			for i in range(10):
-				try:
-					fin.append(f"{c}. {name[i]}{' '*(25-len(name[i]))} - {msg_count[i]}")
-					c += 1
-				except:
-					continue
-			fin = "\n".join(fin)
-			if fin:
-				await ctx.send(f"```css\n{fin}```")
-			else:
-				await ctx.send("No messages yet")
-		# await conn.close()
+            fin = []
+            c = 1
+            
+            for i in range(10):
+                try:
+                    fin.append(f"{c}. {name[i]}{' '*(25-len(name[i]))} - {msg_count[i]}")
+                    c += 1
+                except:
+                    continue
+            fin = "\n".join(fin)
+            if fin:
+                await ctx.send(f"```css\n{fin}```")
+            else:
+                await ctx.send("No messages yet")
+    
+    @commands.command(brief="Get a graph based on the top messages in the guild",
+                      usage="t!graph",
+                      aliases=("topg", "topgraph", "tgraph"))
+    async def graph(self, ctx):
+        try:
+            async with self.db as conn:
+                await conn.execute("""CREATE TABLE IF NOT EXISTS quirks (
+                    username text,
+                    quirk text,
+                    c_spins integer,
+                    uc_spins integer,
+                    r_spins integer,
+                    userid bigint,
+                    current timestamptz,
+                    messages integer,
+                    guild bigint,
+                    yen bigint)""")
+                msgs = await conn.fetch("SELECT userid, messages FROM quirks WHERE guild=$1", ctx.guild.id)
+                msgs = set(msgs)
+                m_count = dict()
+                for i in msgs:
+                    m_count.update({str(self.bot.get_user(list(i.values())[0])): list(i.values())[1]})
+                m_count = {k: m_count[k] for k in sorted(m_count, key=lambda y: m_count[y])}
+                msg_count_y = list(m_count.values())[::-1]
+                name_x = list(m_count.keys())[::-1]
+
+                # plt.style.use("fivethirtyeight")
+                fig, ax = plt.subplots(figsize =(16, 9))
+
+                ax.barh(name_x, msg_count_y, color="#444444", edgecolor="#817E7E")
+                for s in ['top', 'bottom', 'left', 'right']:
+                    ax.spines[s].set_visible(False)
+
+                ax.xaxis.set_ticks_position('none')
+                ax.yaxis.set_ticks_position('none')
+
+                ax.grid(b=True, color='#545454',
+                        linestyle='-.', linewidth=0.5,
+                        alpha=0.2)
+
+                ax.invert_yaxis()
+
+                for i in ax.patches:
+                    plt.text(i.get_width() + 20, i.get_y() + 0.5,
+                             str(round((i.get_width()), 2)),
+                             fontsize=12, fontweight='bold',
+                             color='grey')
+                plt.title(f"Top messages in {ctx.guild.name}")
+                plt.ylabel("Names")
+                plt.xlabel("Message count")
+
+                # plt.tight_layout()
+
+                plt.savefig("images/graph.png")
+
+                await ctx.send(file=discord.File("images/graph.png"))
+        except Exception as e:
+            print(e)
 
 def setup(bot: commands.bot):
-	bot.add_cog(Utility(bot))
+    bot.add_cog(Utility(bot))
